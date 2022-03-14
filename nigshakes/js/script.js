28ec5cf0f08d0aa7ce526f67619fefcead9b12e3
TakakiLeo <leonardo-takaki@hotmail.com>
2022-03-14 10:19:34 -0300

    Projeto NigShakes

---
+++ nigshakes/js/script.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal()
+
+sr.reveal(".box-left",{
+    origin:"left",
+    interval:106,
+    duration:2000,
+    distance:'150%',
+    mobile:false
+})
+
+sr.reveal(".box-right",{
+    origin:"right",
+    interval:106,
+    duration:2000,
+    distance:'10%',
+    mobile:false
+})
+
+sr.reveal(".box-top",{
+    origin:"top",
+    interval:106,
+    duration:2000,
+    distance:'150%',
+    mobile:false
+})
+
+sr.reveal(".box-bottom",{
+    origin:"bottom",
+    interval:106,
+    duration:2000,
+    distance:'150%',
+    mobile:false
+})
